d0db0c014ba413ba1fefefa7c3e7e0e499bb5fa3
jethomas6 <144382158+jethomas6@users.noreply.github.com>
2023-09-08 12:27:55 -0400

    Update home.py

--- Capstone/home.py
+++ Capstone/home.py
@@ -18,7 +18,8 @@ st.write('We have discovered through extensive research that video games have bo
 st.header('The beginning of research into video games and the effects on the brain')
 
 st.write("At the University of Rochester, Daphne Bavelier and C. Shawn Green led the charge on video game research in the late 1990s. They started to investigate the outlandish hypothesis that video games might have an effect on neuroplasticity—a biological process in which the brain adapts and evolves in response to new experiences—and might even help with it.")
-st.header('From start to finish through the project we have come to find out two things. That videogames have positive and negative effects on an adolescents'' psychological state. Games like Fortnite, Call of Duty, and even NBA 2K can leave children with positive traits to transfer to the real world. Games such as Fortnite and Call of Duty which are first and third-person shooters help kids become better at decision making, spatial reasoning, and even attention spans even though to contrary belief of games reducing the attention span of children.')
+st.header("What did we learn?")
+st.write('From start to finish through the project we have come to find out two things. That videogames have positive and negative effects on an adolescents'' psychological state. Games like Fortnite, Call of Duty, and even NBA 2K can leave children with positive traits to transfer to the real world. Games such as Fortnite and Call of Duty which are first and third-person shooters help kids become better at decision making, spatial reasoning, and even attention spans even though to contrary belief of games reducing the attention span of children.')
 st.info('Upload input data in the sidebar to start!')
 
 # checkbox
