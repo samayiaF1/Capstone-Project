72c76230ac8f45fe5fff207eb443c3ccbf270283
Samayia Floyd <88293277+samayiaF1@users.noreply.github.com>
2023-10-05 14:52:53 -0400

    Update home.py

--- Capstone/home.py
+++ Capstone/home.py
@@ -18,43 +18,17 @@ st.write('We have discovered through extensive research that video games have bo
 st.header('The beginning of research into video games and the effects on the brain')
 
 st.write("At the University of Rochester, Daphne Bavelier and C. Shawn Green led the charge on video game research in the late 1990s. They started to investigate the outlandish hypothesis that video games might have an effect on neuroplasticity—a biological process in which the brain adapts and evolves in response to new experiences—and might even help with it.")
+st.header("What negative affects do video games have on adolescents?")
+st.write("As we already know that violent video games can encourage violent and aggressive behavior, but we wanted to know as to why and how do violent video games increase aggression ")
+
 st.header("What did we learn?")
-st.write('From start to finish through the project we have come to find out two things. That videogames have positive and negative effects on an adolescents'' psychological state. Games like Fortnite, Call of Duty, and even NBA 2K can leave children with positive traits to transfer to the real world. Games such as Fortnite and Call of Duty which are first and third-person shooters help kids become better at decision making, spatial reasoning, and even attention spans even though to contrary belief of games reducing the attention span of children.')
-st.info('Upload input data in the sidebar to start!')
 
-# checkbox
-st.checkbox("Disable text input widget", key="disabled")
-st.session_state.disabled
-
-#radio button
-st.radio(
-        "Set text input label visibility 👉",
-        key="visibility",
-        options=["visible", "hidden", "collapsed"],
-)
-st.session_state.visibility
-
-#Scatter plot 
-x1 = [1,2,3,4,5]
-x2 = [10,22,13,34,25]
-y= [100,200,50,60,80]
-fig = plt.figure()
-plt.scatter(x1, x2,
-        c=y, alpha=0.8,
-        cmap='viridis')
-
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.colorbar()
-
-#plt.show()
-st.pyplot(fig)
-
-#sidebar
-dataset_name = st.sidebar.selectbox(
-    'Select Dataset',
-    ('Iris', 'Breast Cancer', 'Wine')
-)
-st.write(f"## {dataset_name} Dataset")
+st.write('From start to finish through the project we have come to find out two things. That video games have positive and negative effects on an adolescents'' psychological state. Games like Fortnite, Call of Duty, and even NBA 2K can leave children with positive traits to transfer to the real world. Games such as Fortnite and Call of Duty which are first and third-person shooters help kids become better at decision making, spatial reasoning, and even attention spans even though to contrary belief of games reducing the attention span of children.')
 
 
+
+
+st.header("Mental Health Exploratory Data Analysis(EDA)")
+
+st.subheader("Database system that shows how mental health has progressed to become better or worst from playing video games")
+st.text("")
